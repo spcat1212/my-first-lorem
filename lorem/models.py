52a5b3df7b9ce47713951feadb8d98f6f5ee01fa
spcat1212 <spcat1212@gmail.com>
2015-07-13 17:16:58 -0400

    added my first template to lorem

--- lorem/models.py
+++ lorem/models.py
@@ -1,10 +1,9 @@
 from django.db import models
 from django.utils import timezone
 
-# Create your models here.
 class Post(models.Model):
     author = models.ForeignKey('auth.User')
-    title = models.CharField(max_length=1000)
+    title = models.CharField(max_length=200)
     text = models.TextField()
     created_date = models.DateTimeField(
         default=timezone.now)
